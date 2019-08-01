298efc10b81e94694bd5d83287f1154f9e7fc2db
@KatKode <sabrinaacm@gmail.com>
2019-08-01 17:39:43 -0500

    slideshow + header, 1er merge

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ const searchTitles = () => {
     const titleName = document.getElementById('byTitle').value;
     const typeOf = type.options[type.selectedIndex].value;
     const year = document.getElementById('byYear').value;
-    let url = `http://www.omdbapi.com/?s=${titleName}&type=${typeOf}&y=${year}&r=json&apikey=3ab3fdc7`
+    let url = `http://www.omdbapi.com/?s=${titleName}&type=${typeOf}&y=${year}&r=json&plot=full&apikey=3ab3fdc7`
     fetch(url)
         .then(res => res.json())
         .then(json => {
@@ -30,10 +30,7 @@ const searchTitles = () => {
                 }]
                 showDisplay(movie);
             });
-
         })
-
-
     display.innerHTML = null;
     return movie;
 };
